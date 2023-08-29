23df378e6fcfa2426a127336eb27e931b83a364c
Gallardo Alejandro Joaquin <97065359+userJoA@users.noreply.github.com>
2023-08-29 04:31:45 -0300

    ac

---
+++ PooPractico2/src/ar/edu/unju/fi/practico2/Punto4/modelo/Empleado.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.practico2.Punto4.modelo;
+
+public class Empleado {
+
+}
--- PooPractico2/src/ar/edu/unju/fi/practico2/punto3/Main.java
+++ PooPractico2/src/ar/edu/unju/fi/practico2/punto3/Main.java
@@ -1,9 +1,33 @@
-package ar.edu.unju.fi.practico2.punto3;
+package ar.edu.unju.fi.practico2.Punto3;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public static List<String> generateSubstrings(String str) {
+        List<String> substrings = new ArrayList<>();
+
+        int n = str.length();
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                substrings.add(str.substring(i, j + 1));
+            }
+        }
+
+        return substrings;
+    }
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String input = "123";
+        List<String> substrings = generateSubstrings(input);
+
+        System.out.println("Substrings of \"" + input + "\":");
+        for (String substring : substrings) {
+            System.out.println(substring);
+        }
 
 	}
 
